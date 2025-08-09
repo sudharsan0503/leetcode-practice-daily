2231fba1affe631a4047b5d90cb54c385beba188
Sudharsan N <sudharsan.exquisite@gmail.com>
2025-08-09 19:22:57 +0530

    Time: 3 ms (65.43%), Space: 26.7 MB (44.51%) - LeetHub

--- 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.cpp
@@ -3,9 +3,16 @@ public:
     string firstPalindrome(vector<string>& words) {
         for(int i=0;i<words.size();i++){
             string s1=words[i];
-            string rev=s1;;
-            reverse(rev.begin(),rev.end());
-            if(rev==s1) return s1;
+            bool flag=true;
+            int low=0;
+            int high=words[i].length()-1;
+            while(low<high){
+                if(words[i][low]!=words[i][high])
+                   flag=false;
+                low++;
+                high--;
+            }
+            if(flag) return s1;
         }
         return "";
     }
