cb7283dcc4ee778c10e17308c742023dbec7e67b
Sudharsan N <sudharsan.exquisite@gmail.com>
2025-08-10 17:54:53 +0530

    Time: 2 ms (55.1%), Space: 8 MB (3.31%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,11 +4,11 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long long low=0;
-        long long high=n-1;
-        long long ans=n;
+        int low=0;
+        int high=n-1;
+        int ans=n;
         while(low<=high){
-            long long mid=low+((high-low)/2);
+            int mid=low+((high-low)/2);
             if(isBadVersion(mid)) 
             {   ans=mid;
                 high=mid-1;
@@ -17,6 +17,6 @@ public:
                 low=mid+1;
             }
         }
-        return ans;
+        return ans;//since 1 based indexing
     }
 };
