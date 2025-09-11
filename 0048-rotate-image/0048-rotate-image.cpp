027f38ceaa06d5563dbd6c3eae02720cddf0d60d
Sudharsan N <sudharsan.exquisite@gmail.com>
2025-09-12 00:25:36 +0530

    Time: 0 ms (100%), Space: 10 MB (99.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,6 +4,7 @@ public:
         int n=matrix.size();
         for(int i=0;i<n-1;i++){
           for(int j=i+1;j<n;j++){
+            if(i!=j)
             swap(matrix[i][j],matrix[j][i]);
           }
         }
